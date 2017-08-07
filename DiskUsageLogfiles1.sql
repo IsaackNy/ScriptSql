dcc4505b270a511caf5427b243b71ea51524344e
IsaackNy <inyobe@yahoo.ca>
2017-08-07 11:00:55 -0400

    Update DiskUsageLogfiles1.sql

--- DiskUsageLogfiles1.sql
+++ DiskUsageLogfiles1.sql
@@ -1,9 +1,17 @@
 --Disk_usage - log files
+DECLARE @tran_log_space_usage as table( database_name sysname, log_size_mb numeric(19,7), log_space_used numeric(8,6), status bit)
 INSERT INTO @tran_log_space_usage 
-EXEC(''DBCC SQLPERF (LOGSPACE) WITH NO_INFOMSGS'');
+EXEC('DBCC SQLPERF (LOGSPACE) WITH NO_INFOMSGS');
+
 SELECT 
     database_name,
     log_size_mb,
     log_space_used,
     status    
 FROM @tran_log_space_usage
+
+
+　
+
+-- Commande 　
+DBCC SQLPERF (LOGSPACE) WITH NO_INFOMSGS
